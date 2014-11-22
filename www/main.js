1affbe1bb7178bd13d9774ce60d53fb216a5e3dd
Claudio Santini <claudio.santini@thomsonreuters.com>
2014-11-22 20:17:23 +0000

    you need more jquery in your life

--- www/main.js
+++ www/main.js
@@ -1,38 +1,43 @@
-
 angular.module('app', [])
-    .controller('MainCtrl', function($scope, $http, $timeout) {
+    .controller('MainCtrl', function ($scope, $http, $timeout) {
 
-        $scope.fishes = [0,1];
+        $scope.fishes = [0, 1];
         $scope.currentFish = 0;
         $scope.currentDuration = 500;
         $scope.actions = ['Flap', 'Forward', 'Dive', 'Rise'];
 
-        $scope.selectFish = function(index){
+        $scope.selectFish = function (index) {
             $scope.currentFish = index;
         };
 
-        $scope.command = function(cmd) {
-            var url = '/arduino/fish-cmd/'+$scope.currentFish+'/'+cmd+'/'+$scope.currentDuration;
+        $scope.command = function (cmd) {
+            var url = '/arduino/fish-cmd/' + $scope.currentFish + '/' + cmd + '/' + $scope.currentDuration;
             $http.get(url)
-                .success(function(data, status, headers) {
-                    console.log(url+' callback');
+                .success(function (data, status, headers) {
+                    console.log(url + ' callback');
                 })
-                .error(function(data, status, headers) {
+                .error(function (data, status, headers) {
                     console.error(url, data);
                 });
         };
 
-        $scope.action = function(act) {
-            var url = '/arduino/fish-action/'+$scope.currentFish+'/'+act;
+        $scope.action = function (act) {
+            var url = '/arduino/fish-action/' + $scope.currentFish + '/' + act;
             $http.get(url);
         };
 
-        $scope.keyboard = function($event) {
-            if(['Up', 'Down', 'Left', 'Right'].indexOf($event.keyIdentifier) !== -1) {
-                $timeout(function() {
-                    document.getElementById($event.keyIdentifier).click();
-                });
-            }
+        $scope.keyboard = function ($event) {
+            $timeout(function () {
+                var match = { '38': 'Up', '37': 'Left', '39': 'Right', '40': 'Down'};
+                var buttonId = match[$event.keyCode];
+                if (buttonId) {
+                    var button = $('#' + buttonId);
+                    button.toggleClass('clicked').click();
+                    $timeout(function() {
+                        button.toggleClass('clicked');
+                    },200);
+                }
+            })
         };
 
     });
